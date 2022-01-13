1d5f532fdeee3aee5a5cd86868643405f03b2209
alexsilva <geniofuturo@gmail.com>
2022-01-13 13:49:01 -0300

    Add renderer atribute.

--- jsonfield/widgets.py
+++ jsonfield/widgets.py
@@ -6,12 +6,12 @@ from jsonfield.utils import default
 
 
 class JSONWidget(forms.Textarea):
-    def render(self, name, value, attrs=None):
+    def render(self, name, value, attrs=None, **kwargs):
         if value is None:
             value = ""
         if not isinstance(value, str):
             value = json.dumps(value, indent=2, default=default)
-        return super(JSONWidget, self).render(name, value, attrs)
+        return super(JSONWidget, self).render(name, value, attrs, **kwargs)
 
 
 class JSONSelectWidget(forms.SelectMultiple):
